d289efe104c82d5d4e801e5fb111b04d89aa4a91
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-11-22 21:11:21 +0200

    javadoc

--- app/src/main/java/com/example/alertdialog/creditsActivity.java
+++ app/src/main/java/com/example/alertdialog/creditsActivity.java
@@ -15,6 +15,12 @@ public class creditsActivity extends AppCompatActivity
         setContentView(R.layout.activity_credits);
     }
 
+    /**
+     * This method runs when the back button is pressed. It goes back to the main activity
+     * 
+     * @param view
+     */
+    
     public void back(View view) 
     {
         finish();
