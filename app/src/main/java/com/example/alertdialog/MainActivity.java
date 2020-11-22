9601696ff7df54011734be2d6e3ca8f2374960fb
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-11-22 19:44:55 +0200

    Button3 part 2

--- app/src/main/java/com/example/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.alertdialog;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
@@ -32,6 +33,24 @@ public class MainActivity extends AppCompatActivity
         adb.setTitle("Message");
         adb.setMessage("This AlertDialog has a message and an image");
         adb.setIcon(R.drawable.image);
+        AlertDialog ad=adb.create();
+        ad.show();
+    }
+
+    public void button3Clicked(View view)
+    {
+        adb=new AlertDialog.Builder(this);
+        adb.setTitle("Message");
+        adb.setMessage("This AlertDialog has a message, an image, and a button");
+        adb.setIcon(R.drawable.image);
+        adb.setPositiveButton("Close", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+
+            }
+        });
         AlertDialog ad=adb.create();
         ad.show();
     }
