e1de4d823139bbdcf96ec5f797ff495d02f51469
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-11-22 19:53:42 +0200

    Button4 part 2

--- app/src/main/java/com/example/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -6,16 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
     AlertDialog.Builder adb;
+    LinearLayout layout;
+    Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout=(LinearLayout)findViewById(R.id.layout);
+        rnd=new Random();
     }
 
     public void button1Clicked(View view)
@@ -49,6 +57,24 @@ public class MainActivity extends AppCompatActivity
             public void onClick(DialogInterface dialog, int which)
             {
 
+            }
+        });
+        AlertDialog ad=adb.create();
+        ad.show();
+    }
+
+    public void button4Clicked(View view)
+    {
+        adb=new AlertDialog.Builder(this);
+        adb.setTitle("Message");
+        adb.setMessage("This AlertDialog has a message, an image, and 2 buttons. Do you want to change the background color?");
+        adb.setIcon(R.drawable.image);
+        adb.setPositiveButton("Random Color", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                layout.setBackgroundColor(rnd.nextInt());
             }
         });
         AlertDialog ad=adb.create();
