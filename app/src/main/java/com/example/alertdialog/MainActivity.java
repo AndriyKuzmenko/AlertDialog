0888a9e324b0c240c0a0731323362c409603ced0
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-11-22 19:22:11 +0200

    Button1 part 2

--- app/src/main/java/com/example/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    AlertDialog.Builder adb;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void button1Clicked(View view)
+    {
+        adb=new AlertDialog.Builder(this);
+        adb.setTitle("Message");
+        adb.setMessage("This AlertDialog has a message");
+        AlertDialog ad=adb.create();
+        ad.show();
     }
 }
